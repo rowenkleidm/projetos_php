43b059e362ef08542bfd5908417cecd5700a8035
rowenkleidm <guitarxxx20@gmail.com>
2021-03-13 13:24:24 -0300

    Update index.php

--- Aula_04/index.php
+++ Aula_04/index.php
@@ -7,11 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-        <h2>Meu Form</h2>
-        <form action="">
-            <input type="text" name="<?php $n ?>" placeholder="seu nome aqui..." />
-            <input type="submit" name="acao" value="Enviar" />
-        </form>
+        
         <pre>
         <?php
             require_once 'Pessoa.php';
